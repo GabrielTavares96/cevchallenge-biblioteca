23097b6723058ec7c7c3560712b0b02c04875e07
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-07-21 22:07:09 -0300

    First commit

---
+++ src/main/java/com/devchallenge/biblioteca/DevchallengeBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.devchallenge.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevchallengeBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevchallengeBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devchallenge/biblioteca/DevchallengeBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchallenge.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevchallengeBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
